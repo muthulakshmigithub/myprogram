e48ae8f1170a4ec81591d094b3f9e237d1a56963
muthulakshmigithub <98873386+muthulakshmigithub@users.noreply.github.com>
2022-02-03 21:09:51 +0530

    Add files via upload

---
+++ Program2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i=1;i<=10;i++)
+		{
+			System.out.println(i +" ");
+		}
+
+	}
+
+}
